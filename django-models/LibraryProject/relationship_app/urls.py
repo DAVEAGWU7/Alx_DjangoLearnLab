acc9c1acbb35ef2fab268ba4f84f16ff6bb5d21d
David Agwu <okoriedavid930@gmail.com>
2025-11-09 14:02:17 +0100

    Add user authentication views, URLs, and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,3 +5,13 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # FBV
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV
 ]
+
+
+
+from . import views
+
+urlpatterns += [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+]
