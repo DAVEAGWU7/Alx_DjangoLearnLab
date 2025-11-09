7609871ec55d92cdd3e8bb8024ca16a3784bf061
David Agwu <okoriedavid930@gmail.com>
2025-11-09 11:28:05 +0100

    Add sample queries for author, library, and librarian

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,28 +1,22 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Create some authors
-author1 = Author.objects.create(name="George Orwell")
-author2 = Author.objects.create(name="J.K. Rowling")
+# -----------------------------
+# Query all books by a specific author
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(books_by_author)
 
-# Create some books
-book1 = Book.objects.create(title="1984", author=author1)
-book2 = Book.objects.create(title="Harry Potter", author=author2)
+# -----------------------------
+# List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(books_in_library)
 
-# Create a library and add books
-library = Library.objects.create(name="Central Library")
-library.books.add(book1, book2)
-
-# Create a librarian
-librarian = Librarian.objects.create(name="Alice", library=library)
-
-# Queries
-# 1. All books by George Orwell
-books_by_orwell = Book.objects.filter(author__name="George Orwell")
-print(books_by_orwell)
-
-# 2. All books in Central Library
-all_books_in_library = library.books.all()
-print(all_books_in_library)
-
-# 3. Retrieve the librarian of Central Library
-print(library.librarian)
+# -----------------------------
+# Retrieve the librarian for a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library)
+print(librarian)
