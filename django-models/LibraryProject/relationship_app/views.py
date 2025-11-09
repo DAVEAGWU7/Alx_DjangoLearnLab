b4d449303f578b35d17aed51f02b1e3fbd5abce0
David Agwu <okoriedavid930@gmail.com>
2025-11-09 19:46:59 +0100

    Add class-based LibraryDetailView and template

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -63,3 +63,26 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library, Book
+
+# Function-based view (already present earlier)
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# ✅ Class-based view for specific library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
+
