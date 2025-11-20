dfc34daf560af59cc582261b508de2e9adf0b3e2
David Agwu <okoriedavid930@gmail.com>
2025-11-20 21:15:12 +0100

    Fix Book model and migrations

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -3,6 +3,15 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+    published = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book records"),
+            ("can_create", "Can create book records"),
+            ("can_edit", "Can edit book records"),
+            ("can_delete", "Can delete book records"),
+        ]
 
     def __str__(self):
         return self.title
