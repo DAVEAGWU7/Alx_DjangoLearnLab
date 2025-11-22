df45707814741e8d0ac87a2e9254f570ff6d8c5d
David Agwu <okoriedavid930@gmail.com>
2025-11-22 08:18:11 +0100

    Add BookList API endpoint with DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,5 @@
 from django.urls import path
-from .views import BookListCreateView, BookRetrieveUpdateDeleteView
-
-urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookRetrieveUpdateDeleteView.as_view(), name='book-detail'),
-]
-
-
-from django.urls import path
-from .views import BookList
+from .views import BookList  # import the correct view
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,23 +2,6 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-
-class BookListCreateView(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-
-class BookRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-from rest_framework.generics import ListAPIView
-from .models import Book
-from .serializers import BookSerializer
-
-class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
- 
