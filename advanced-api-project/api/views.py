a840cb14777d25f3bf4f51942cf642b0c23311ea
David Agwu <okoriedavid930@gmail.com>
2025-11-27 00:41:16 +0100

    Added generic views, permissions, URL patterns for CRUD operations

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
+# PUBLIC — Anyone can GET
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -13,6 +14,7 @@ class BookDetailView(generics.RetrieveAPIView):
     serializer_class = BookSerializer
     permission_classes = [AllowAny]
 
+# PROTECTED — Only authenticated users can POST/PUT/DELETE
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
