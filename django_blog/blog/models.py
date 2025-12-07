4c5b43452427ec7126f0a18dc7937e120812a545
daveagwu7 <okoriedavid930@gmail.com>
2025-12-07 15:54:13 +0100

    Added USER and PORT fields to DATABASES config to satisfy checker

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,36 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse("post-detail", kwargs={"pk": self.pk})
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Comment by {self.author} on {self.post}"
-
-    def get_absolute_url(self):
-        return reverse("post-detail", kwargs={"pk": self.post.pk})
